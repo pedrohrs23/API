21d285c5baf8a0b2b6b2bb9e2513c80aebf21997
Pedro Henrique <pedrohenrique170496@gmail.com>
2022-11-21 03:31:10 -0300

    Modificações 0

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const routes = require('./config/routes')
 
+/* server.listen(3000, () => {
+    console.log('Servidor está funcionando...')
+}); */
+
 const app = express()
 
 app.use(morgan('dev'))
@@ -15,6 +19,6 @@ app.use(cors())
 app.use(routes)
 
 app.listen(21262, () => {
-    console.log('Express started at http://localhost:21262')
+    console.log('Servidor está funcionando... http://localhost:21262')
 })
 
